a90a0acaae7ac0a778fa06979cfec46d04515985
yvpengli <bu_zhi_dao_2006@126.com>
2022-04-03 20:36:36 +0800

    实现sph，遇到了碰到边界会飞走的bug

--- src/include/particle_system.h
+++ src/include/particle_system.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <iostream>
 #include "sphere.h"
+#include <map>
+#include <tuple>
 
 class ParticleSystem {
 
@@ -17,6 +19,9 @@ public:
         particle_diameter = 2 * particle_radius;
         particle_num = 0;
         m_V = 0.8 * pow(particle_diameter, 3.0f);
+        support_radius = particle_radius * 4.0;
+
+        grid_size = support_radius;
 
     }
 
@@ -91,6 +96,48 @@ public:
         v[p_i] -= (1.0f + c_f) * glm::dot(v[p_i], dir) * dir;
     }
 
+    glm::ivec3 pos_to_index( int p_i ) {
+        int x_offset = static_cast<int>( x[p_i][0] / grid_size );
+        int y_offset = static_cast<int>( x[p_i][1] / grid_size );
+        int z_offset = static_cast<int>( x[p_i][2] / grid_size );
+        return glm::ivec3(x_offset, y_offset, z_offset);
+    }
+
+    void initialize_grid() {
+        grid.clear();
+        for (int i = 0; i < particle_num; i ++) {
+            glm::ivec3 index = pos_to_index(i);
+            std::tuple<int, int, int> tp = {index[0], index[1], index[2]};
+            grid[ tp ].push_back(i);
+        }
+    }
+
+    void search_neighbors() {
+        particle_neighbors.swap(std::vector<std::vector<int>>(particle_num));
+        for (int i = 0; i < particle_num; i ++) {
+            glm::ivec3 center = pos_to_index(i);
+            for (int x_offset = -1; x_offset < 2; x_offset ++)
+            for (int y_offset = -1; y_offset < 2; y_offset ++)
+            for (int z_offset = -1; z_offset < 2; z_offset ++) {
+                std::tuple<int, int, int> cell = {center[0] + x_offset, center[1] + y_offset, center[2] + z_offset};
+                vector<int> pos_in_grid = grid[cell];
+                for (int p_i : pos_in_grid) {
+                    glm::vec3 pos = x[p_i];
+                    float distance = glm::length( x[i] - pos );
+                    if ( p_i != i && distance < support_radius ) {
+                        particle_neighbors[i].push_back(p_i);
+                    }
+                }
+            }
+        }
+    }
+
+
+    void initialize_particle_system() {
+        initialize_grid();
+        search_neighbors();
+    }
+
 public:
     float system_length;
     float screen_to_world_ratio;
@@ -102,13 +149,23 @@ public:
     int particle_max_num;
     int particle_num;
     float m_V;
+    float support_radius;
+
+    // int particle_max_num;
+    // int particle_max_num_per_cell;
+    // int particle_max_num_neighbor;
+
     // Single Particle Properties
     std::vector<glm::vec3> x;       
     std::vector<glm::vec3> v;
+    std::vector<float> density;
+    std::vector<float> pressure;
+    std::vector<std::vector<int>> particle_neighbors;
+
+    // Grid related properties
+    float grid_size;
+    std::map<std::tuple<int, int, int>, std::vector<int>> grid; 
 
     // DataOutput
     std::vector<glm::vec3> position_and_normal;
-
-
-
 };
--- src/include/wcsph_solver.h
+++ src/include/wcsph_solver.h
@@ -31,12 +31,91 @@ public:
         // std::cout << "first point location: " << ps.position_and_normal[0][0] << " " << ps.position_and_normal[0][1] << " " << ps.position_and_normal[0][2] << std::endl;
     }
 
+    float cubic_kernel( float r_norm ) {
+        float res = 0.0f;
+        float h = ps.support_radius;
+
+        float k = 8 / (PI * powf( h , 3.0f) );
+        float q = r_norm / h;
+        if ( q <= 1.0 ) {
+            if ( q <= 0.5 ) {
+                float q2 = q * q;
+                float q3 = q2 * q;
+                res = k * (6.0f * q3 - 6.0f * q2 + 1);
+            } else {
+                res= k * 2 * powf(1 - q, 3.0f);
+            }
+        }
+        return res;
+    }
+
+    glm::vec3 cubic_kernel_derivative(glm::vec3 r) {
+        float h = ps.support_radius;
+        float k = 8.0f / PI;
+        k = 6.0f * k / powf( h, 3.0f );
+        float r_norm = glm::length(r);
+        float q = r_norm / h;
+        glm::vec3 res = glm::vec3(0.0f);
+
+        if ( r_norm > 1e-5 && q <= 1.0f ) {
+            glm::vec3 grad_q = r / (r_norm * h);
+            if (q <= 0.5) {
+                res = k * q * (3.0f * q - 2.0f) * grad_q;
+            } else {
+                float factor = 1.0f - q;
+                res = k * (-factor * factor) * grad_q;
+            }
+        }
+        return res;
+    }
+
+    glm::vec3 pressure_force(int p_i, int p_j, glm::vec3 r) {
+        glm::vec3 res = -density_ref * ps.m_V * ( powf( ps.pressure[p_i] / ps.density[p_i], 2.0f)
+                + powf(ps.pressure[p_j] / ps.density[p_j], 2.0f) ) 
+                * cubic_kernel_derivative(r);
+        return res;
+    }
+
+    void compute_density() {
+        ps.density.swap(std::vector<float>(ps.particle_num, 0.0f));
+        for (int i = 0; i < ps.particle_num; i ++) {
+            glm::vec3 x_i = ps.x[i];
+            for ( int p_j : ps.particle_neighbors[i] ) {
+                glm::vec3 x_j = ps.x[p_j];
+                ps.density[i] += ps.m_V * cubic_kernel(glm::length(x_i - x_j));
+            }
+            ps.density[i] *= density_ref;
+        }
+    }
+
+    void compute_pressure_forces() {
+        ps.pressure.swap(std::vector<float>(ps.particle_num, 0.0f));
+        for (int i = 0; i < ps.particle_num; i ++) {
+            ps.density[i] = max(ps.density[i], density_ref);
+            ps.pressure[i] = stiffness * ( powf( ps.density[i] / density_ref, exponent) - 1.0f );
+        }
+
+        for (int i = 0; i < ps.particle_num; i ++) {
+            glm::vec3 x_i = ps.x[i];
+            glm::vec3 d_v = glm::vec3(0.0f);
+            for ( int p_j : ps.particle_neighbors[i] ) {
+                glm::vec3 x_j = ps.x[p_j];
+                d_v += pressure_force(i, p_j, x_i - x_j);
+            }
+            d_velocity[i] += d_v;
+        }
+
+    }
+
     void substep() {
+        compute_density();
         compute_non_pressure_forces();
+        compute_pressure_forces();
         advert();
     }
 
     void step() {
+        ps.initialize_particle_system();
         // update x[] and v[] by forces
         substep();
         // boundary test
@@ -48,10 +127,11 @@ public:
     float g = -9.8f;
     float viscosity = 0.05f;
     float density_ref = 1000.0f;
+    float stiffness = 50.0f;
+    float exponent = 7.0f;
     float mass;
     float dt = 2e-4;
+    const float PI = 3.14159265358979323846;
 
-    float exponent = 7.0f;
-    float stiffness = 50.0f;
     std::vector<glm::vec3> d_velocity;
 };
